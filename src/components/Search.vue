14b543b7526606669fe29247b6a23b6238ed205e
eunjae-lee <karis612@gmail.com>
2021-07-21 10:43:22 +0200

    add search

---
+++ src/components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <ais-instant-search :search-client="searchClient" index-name="instant_search">
+    <ais-search-box />
+    <ais-stats />
+    <ais-refinement-list attribute="brand" />
+    <ais-hits>
+      <template v-slot:item="{ item }">
+        <p>
+          <ais-highlight attribute="name" :hit="item" />
+        </p>
+        <p>
+          <ais-highlight attribute="brand" :hit="item" />
+        </p>
+      </template>
+    </ais-hits>
+    <ais-pagination />
+  </ais-instant-search>
+</template>
+
+<script>
+import {
+  AisInstantSearch,
+  // AisInstantSearchSsr,
+  AisRefinementList,
+  AisHits,
+  AisHighlight,
+  AisSearchBox,
+  AisStats,
+  AisPagination
+} from "vue-instantsearch/dist/vue3/es";
+import algoliasearch from "algoliasearch/lite";
+const searchClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+export default {
+  components: {
+    AisInstantSearch,
+    // AisInstantSearchSsr,
+    AisRefinementList,
+    AisHits,
+    AisHighlight,
+    AisSearchBox,
+    AisStats,
+    AisPagination
+  },
+  data() {
+    return {
+      searchClient
+    };
+  }
+};
+</script>
