58e6937cda65ff3bdc3b2a3ea19b2b160d47b162
eunjae-lee <karis612@gmail.com>
2021-07-23 16:58:41 +0200

    move server.js to root

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const manifest = require("../dist/server/ssr-manifest.json");
+const manifest = require("./dist/server/ssr-manifest.json");
 
 const server = express();
 
-const appPath = path.join(__dirname, "../dist/server", manifest["app.js"]);
+const appPath = path.join(__dirname, "./dist/server", manifest["app.js"]);
 const render = require(appPath).render;
 const indexTemplate = fs.readFileSync(
-  path.join(__dirname, "../dist/client", "index.html"),
+  path.join(__dirname, "./dist/client", "index.html"),
   "utf-8"
 );
 
 server.use(
   "/img",
-  express.static(path.join(__dirname, "../dist/client", "img"))
+  express.static(path.join(__dirname, "./dist/client", "img"))
 );
-server.use("/js", express.static(path.join(__dirname, "../dist/client", "js")));
+server.use("/js", express.static(path.join(__dirname, "./dist/client", "js")));
 server.use(
   "/css",
-  express.static(path.join(__dirname, "../dist/client", "css"))
+  express.static(path.join(__dirname, "./dist/client", "css"))
 );
 server.use(
   "/favicon.ico",
-  express.static(path.join(__dirname, "../dist/client", "favicon.ico"))
+  express.static(path.join(__dirname, "./dist/client", "favicon.ico"))
 );
 
 server.get("*", async (req, res) => {
